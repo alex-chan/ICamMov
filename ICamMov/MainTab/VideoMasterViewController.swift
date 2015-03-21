246eb9992fdc64daad55f1e4021612f5fc99f6aa
alex-chan <czzsunset@gmail.com>
2015-03-21 17:34:06 +0800

    main video page improve

--- ICamMov/MainTab/VideoMasterViewController.swift
+++ ICamMov/MainTab/VideoMasterViewController.swift
@@ -11,29 +11,37 @@ import Foundation
 class VideoMasterViewController: UITableViewController, UITableViewDataSource, UITableViewDelegate {
 
     
-    var hasVideoData = false
+    var hasVideoData = true
+    var isRefreshingVideos = false
+    var videos : [AnyObject] = [["createdAt":"2323-323-1"]]
+    
     
     @IBOutlet weak var refreshNewVideoControl: UIRefreshControl!
+    
     // MARK: Override functions
     
     override func viewDidLoad(){
         
         super.viewDidLoad()
         
-        
-        self.setupRefreshControl()
-        
+        self.tableView.delegate = self
         
         
     }
     
     
     
+    
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        if hasVideoData {
+        
+        print("number of sections:")
+        
+        if self.hasVideoData {
+            println("1")
             return 1
         }else{
-            
+            println("0")
             var label = UILabel(frame: CGRectMake(0,0,self.view.bounds.size.width, self.view.bounds.size.height))
             label.text = "没有视频喔，请下拉刷新"
             label.textColor = UIColor.blackColor()
@@ -48,30 +56,58 @@ class VideoMasterViewController: UITableViewController, UITableViewDataSource, U
             
         }
     }
+    
+    
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        println("number of rows: \(countElements(self.videos))")
+        println("number of rows: \(countElements(videos))")
+        return countElements(self.videos)
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        println("cellForRowAtIndexPath")
+        
+        var cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "video_cell")
+        
+        var obj = self.videos[indexPath.row] as [String: AnyObject]
+        cell.textLabel?.text = obj["createdAt"] as? String
+        return cell
+    }
+    
+    
+    // MARK: Actions
     @IBAction func refreshVideos(sender: UIRefreshControl) {
-        if let refreshCtrl =  self.refreshControl {
-            if refreshCtrl.refreshing{
-                
-//                println("end refreshing")
-//                refreshCtrl.endRefreshing()
-                
-            }else{
-            
-                println("refresh ")
-                AVCloud.callFunctionInBackground("refreshVideos", withParameters: ["test":"test"], target: self, selector: "refreshVideoResult:")
-                
-            }
+        println("refresh ")
+        if !self.isRefreshingVideos {
+            // Selector Problem :http://stackoverflow.com/questions/24007650/selector-in-swift
+            AVCloud.callFunctionInBackground("refreshVideos", withParameters: nil, target: self, selector: "refreshVideoResult:error:")
+            self.isRefreshingVideos = true
         }
+        
     }
     
     // MARK: Selectors
-    func refreshVideoResult(result: AnyObject, error: NSError!){
+    func refreshVideoResult(result: AnyObject!, error: NSError!){
+        
+        self.isRefreshingVideos = false
+        
         println("refreshVideoResult:")
         if error == nil{
-            println("result:\(result)")
+            self.hasVideoData = true
+//            self.videos = result["result"]
+            println(result["result"])
+//            println(result["result"][0])
+//            println(result["result"][0]["createdAt"])
+            
+            self.tableView.reloadData()
+            
+//            println("result:\(result)")
             
         }else{
             println(error.localizedDescription)
+            Utils.alert(message: error.localizedDescription)
         }
         
         self.refreshControl!.endRefreshing()
@@ -80,9 +116,7 @@ class VideoMasterViewController: UITableViewController, UITableViewDataSource, U
     }
     
     // MARK: Custom functions
-    func setupRefreshControl(){
-        
-    }
+
     
 }
 
