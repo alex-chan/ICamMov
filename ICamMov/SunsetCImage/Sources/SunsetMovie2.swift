c03e4f727111c9977a837f625b1cb3d74a35549e
alex-chan <czzsunset@gmail.com>
2015-04-25 14:19:51 +0800

    Add SunsetMovie2

---
+++ ICamMov/SunsetCImage/Sources/SunsetMovie2.swift
@@ -0,0 +1,54 @@
+//
+//  SunsetMovie2.swift
+//  ICamMov
+//
+//  Created by Alex Chan on 15/4/23.
+//  Copyright (c) 2015年 sunset. All rights reserved.
+//
+
+import Foundation
+
+class SunsetMovie2: SunsetOutput {
+    
+    var url: NSURL!
+    var asset: AVAsset!
+    
+    var reader: AVAssetReader?
+    
+    init(URL: NSURL){
+        super.init()
+        self.url = URL
+    }
+    
+    override func start() {
+        var inputOptions = [AVURLAssetPreferPreciseDurationAndTimingKey: NSNumber(bool: true)]
+        var inputAsset = AVURLAsset(URL: url, options: inputOptions)
+        inputAsset.loadValuesAsynchronouslyForKeys(["tracks"], completionHandler: {
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+                var error: NSError?
+                var tracksStatus = inputAsset.statusOfValueForKey("tracks", error: &error)
+                if tracksStatus != AVKeyValueStatus.Loaded {
+                    return
+                }
+                self.asset = inputAsset
+                self.processAsset()
+            })
+            return
+        })
+    }
+    
+    func processAsset(){
+        reader = self.createAssetReader()
+        
+    }
+    
+    func createAssetReader() -> AVAssetReader {
+        var error: NSError?
+        var assetReader = AVAssetReader(asset: asset, error: &error)
+        var outputSettings = [kCVPixelBufferPixelFormatTypeKey: kCVPixelFormatType_420YpCbCr8BiPlanarFullRange]
+        var readerVideoOuput = AVAssetReaderVideoCompositionOutput(videoTracks: <#[AnyObject]!#>, videoSettings: outputSettings)
+        
+        
+    }
+    
+}
